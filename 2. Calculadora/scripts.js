3dac8d78fb8a9df47de59aaccdbb7a5191fd810c
Daniel <55301388+danielfarah54@users.noreply.github.com>
2021-04-29 19:19:00 -0300

    Update scripts.js

--- 2. Calculadora/scripts.js
+++ 2. Calculadora/scripts.js
@@ -2,12 +2,13 @@ var primeiroValor = parseInt(prompt("Digite o primeiro valor:"))
 var segundoValor = parseInt(prompt("Digite o segundo valor:"))
 
 var resultado
-
+var h2 = document.getElementById('resultado')
 var operacao = prompt("Digite 1 para divisão, 2 para multiplicação, 3 para soma ou 4 para subtração:")
 
 if(operacao == 1){
   resultado = primeiroValor / segundoValor
-  document.write("<h2>" + primeiroValor + " / " + segundoValor + " = " + resultado + "</h2>")
+  
+  h2.innerHTML = "<h2>" + primeiroValor + " / " + segundoValor + " = " + resultado + "</h2>"
 }
 else if(operacao == 2){
   resultado = primeiroValor * segundoValor
