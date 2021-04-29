e98f0bb8ebe150c13371c1349a95f43b2d13fd34
Daniel <55301388+danielfarah54@users.noreply.github.com>
2021-04-29 19:33:41 -0300

    Update scripts.js

--- 2. Calculadora/scripts.js
+++ 2. Calculadora/scripts.js
@@ -5,7 +5,7 @@ var resultado
 var h2 = document.getElementById("resultado")
 //var operacao = prompt("Digite 1 para divisão, 2 para multiplicação, 3 para soma ou 4 para subtração:")
 
-h2.innerHTML = "<h2>" + 2 + " / " + 2 + " = " + 1 + "</h2>"
+h2.innerHTML = "<h2> 2 / 2 = 1 </h2>"
 
 if(operacao == 1){
   resultado = primeiroValor / segundoValor
